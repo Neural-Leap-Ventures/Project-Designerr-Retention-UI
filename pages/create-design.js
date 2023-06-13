f3d33dd979a70a267e220c954999bf21f8389275
William-Boomer-Baker <boomer@boomerbaker.com>
2023-06-14 02:06:49 +0530

    Fix update remove unwanted scripts

--- pages/create-design.js
+++ pages/create-design.js
@@ -17,7 +17,25 @@ export default function Signup() {
           crossorigin="anonymous"
         />
       </Head>
-      
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+        integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"
+        integrity="sha384-fbbOQedDUMZZ5KreZpsbe1LCZPVmfTnH7ois6mU1QK+m14rQ1l2bGBq41eYeM/fS"
+        crossOrigin="anonymous"
+        strategy="afterInteractive"
+      />
+
       <div>
         <div className="container-fluid" style={{ backgroundColor: "#F7F9FF" }}>
           {/* <div className="header p-2" style={{ backgroundColor: "#F7F9FF" }}>
